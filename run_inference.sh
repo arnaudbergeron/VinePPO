a25bf2abb5c94a94157670183781c11cde6b17f0
arnaudbergeron <58529583+arnaudbergeron@users.noreply.github.com>
2024-12-21 13:35:57 -0500

    Update run_inference.sh

--- run_inference.sh
+++ run_inference.sh
@@ -3,7 +3,6 @@ CONFIGSTR="configs/$2"
 
 export APP_SEED="2746318213"
 export WANDB_RUN_ID="$1"
-export WANDB_API_KEY="35f62c8cac8a9711b806c204b72ed940e23fca82"
 export OUTLINES_CACHE_DIR="experiments/$1"
 export XDG_CACHE_HOME="experiments/$1"
 
